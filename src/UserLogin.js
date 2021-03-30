b2fd5668e439227e715f016b8f8a642535f6a312
Juliano soder <julianosoder1989@gmail.com>
2021-03-30 17:55:49 -0300

    still at the same inssue, tring to convert the response

--- src/UserLogin.js
+++ src/UserLogin.js
@@ -4,52 +4,45 @@ import { Redirect } from 'react-router-dom'
 
 const UserLogin = () => {
 
-    async function  loginJson(){
+    const loginJson = () => {
 
         try {
             const recipeUrl = 'http://localhost:5000/login';
             const postBody = {
                 name: document.getElementById("nameInput").value,
-                    password: document.getElementById("pass").value
+                password: document.getElementById("pass").value
             };
             const requestMetadata = {
                 dataType: 'json',
                 method: 'POST',
-                mode:'no-cors',
+                mode:'cors',
                 headers: {
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify(postBody)
             };
-                fetch(recipeUrl, requestMetadata)
-                .then(response => response.json())
-                .then(value => console.log(value))
-                        
-          /*  fetch('http://localhost:5000/login', {
-                mode:'no-cors',
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json'
-              },
-              body: JSON.stringify({
-                name: document.getElementById("nameInput").value,
-                password: document.getElementById("pass").value,
-              })
-            })
-            .then(async function (response, err) {
-                if(err){
-                    console.log(err)
-                }
-                    const data =await response.json();
-                    console.log(data);
+            fetch(recipeUrl, requestMetadata)
+                .then((response) => {
+                    response.text()
+                        .then((text) => {
+                            if(text){
+                               JSON.parse(text)
+                            }else{}
+                        })
                 })
-          */
+                .then((json) => {
+                    console.log(json)
+                    // localStorage.setItem("user_name", JSON.parse(text).user_name);
+                    // console.log(localStorage.getItem("user_name"))
+
+                }).catch(error => console.log('Oops, error:', error))
+
         } catch (error) {
             console.log(error)
         }
 
-}
-        
+    }
+
     const [getAccount, setNeedAccount] = useState(false)
     const goToCreateAccount = () => {
         return <Redirect to="/CreateAccount" />
@@ -62,23 +55,23 @@ const UserLogin = () => {
         return (
             <div className="main-div-body">
                 <div className="login">
-                <h2>Login into game Infinite Dungeon.</h2>
-                <h4>User Name</h4>
-                <input id="nameInput" onChange={() => { document.getElementById("nameLogin").textContent = "welcome " + document.getElementById("nameInput").value + "."; }} type="text" />
-                <h4>User Password</h4>
-                <input type="password" id="pass" /><br /><br />
-                <button id='login-bnt' onClick={() =>loginJson()}>Login</button>
-                <p id="nameLogin"></p>
-                <br />
-                <a className="a-createAcc" onClick={() => setNeedAccount(true)}>I don`t have account..click here</a>
-            </div>
+                    <h2>Login into game Infinite Dungeon.</h2>
+                    <h4>User Name</h4>
+                    <input id="nameInput" onChange={() => { document.getElementById("nameLogin").textContent = "welcome " + document.getElementById("nameInput").value + "."; }} type="text" />
+                    <h4>User Password</h4>
+                    <input type="password" id="pass" /><br /><br />
+                    <button id='login-bnt' onClick={loginJson}>Login</button>
+                    <p id="nameLogin"></p>
+                    <br />
+                    <a className="a-createAcc" onClick={() => setNeedAccount(true)}>I don`t have account..click here</a>
+                </div>
             </div>
         )
     }
 
     const render = () => {
-        if (getAccount==true) {
-            return(goToCreateAccount());
+        if (getAccount == true) {
+            return (goToCreateAccount());
         } else {
             return (
                 isLogged ? loggedComponent() : login()
